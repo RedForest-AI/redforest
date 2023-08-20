b7a36ec52fe466d0c19002572134405c3b5d1b30
edavalosanaya <davalosaeduardo@gmail.com>
2023-08-20 10:47:34 -0500

    WIP(expo with web keycloak): Changed to expo and made web version work with keycloak.

---
+++ RedForest/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import fetchTokens from './keycloak';
+
+export default function App() {
+  const [tokens, setTokens] = useState<{ accessToken?: string, refreshToken?: string }>({});
+
+  useEffect(() => {
+    async function getTokens() {
+      try {
+        const fetchedTokens = await fetchTokens();
+        setTokens(fetchedTokens);
+      } catch (error) {
+        console.error("Error fetching tokens:", error);
+      }
+    }
+
+    getTokens();
+  }, []);  // The empty array means this useEffect will run once when the component mounts.
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      {tokens.accessToken && <Text>Access Token: {tokens.accessToken}</Text>}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
